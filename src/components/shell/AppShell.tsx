c285351d07feab1b73039b9cd264008569647f32
adizalmumtaz30 <adizalmumtaz30@gmail.com>
2026-09-21 18:44:46 +0700

    feat: mount mobile sheet navigation in app shell

--- src/components/shell/AppShell.tsx
+++ src/components/shell/AppShell.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import { Sidebar } from "@/components/shell/Sidebar";
+import { MobileSidebar, Sidebar } from "@/components/shell/Sidebar";
 import { TopBar } from "@/components/shell/TopBar";
 import { MegaMendung } from "@/components/shell/MegaMendung";
 import type { School } from "@/lib/domain/school";
@@ -20,7 +20,7 @@ export function AppShell({
 }: AppShellProps) {
   return (
     <div className="flex h-dvh bg-canvas">
-      <Sidebar />
+      <Sidebar />\n      <MobileSidebar />
       <div className="flex min-w-0 flex-1 flex-col">
         <TopBar school={school} academicYear={academicYear} academicYears={academicYears} />
         <main className="relative flex-1 overflow-y-auto">
